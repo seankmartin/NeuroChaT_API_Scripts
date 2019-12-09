b09bfcc9083c0d8204b00bbb0d57b174846aa6fc
Sean Martin <martins7@tcd.ie>
2019-12-09 17:21:12 +0000

    Added plotting lfp function Facitlitates visualising areas of the lfp over different time scales

---
+++ analysis/general_lfp.py
@@ -0,0 +1,61 @@
+import math
+import os
+from collections import OrderedDict
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from neurochat.nc_lfp import NLfp
+
+
+def get_all_lfp(filename, channels="all"):
+    """Return an orderedDict of lfp objects, one for each channel."""
+    lfp_odict = OrderedDict()
+    if channels == "all":
+        channels = [i + 1 for i in range(32)]
+
+    for i in channels:
+        end = ".eeg"
+        if i != 1:
+            end = end + str(i)
+        load_loc = filename + end
+        lfp = NLfp(system="Axona")
+        lfp.load(load_loc)
+        lfp_odict[str(i)] = lfp
+    return lfp_odict
+
+
+def plot_lfp(
+        out_dir, lfp_odict, segment_length=150, in_range=None, dpi=50):
+    """
+    Create a number of figures to display lfp signal.
+
+    There will be one figure for each split of the
+    total length into segment_length and a row for each value in lfp_odict.
+    """
+    if in_range is None:
+        in_range = (0, max([lfp.get_duration() for lfp in lfp_odict.values()]))
+    y_axis_max = max([max(lfp.get_samples()) for lfp in lfp_odict.values()])
+    y_axis_min = min([min(lfp.get_samples()) for lfp in lfp_odict.values()])
+    for split in np.arange(in_range[0], in_range[1], segment_length):
+        fig, axes = plt.subplots(
+            nrows=len(lfp_odict),
+            figsize=(20, len(lfp_odict) * 2))
+        a = np.round(split, 2)
+        b = np.round(min(split + segment_length, in_range[1]), 2)
+        out_name = os.path.join(
+            out_dir, "{}s_to_{}s.png".format(a, b))
+        for i, (key, lfp) in enumerate(lfp_odict.items()):
+            convert = lfp.get_sampling_rate()
+            c_start, c_end = math.floor(a * convert), math.floor(b * convert)
+            lfp_sample = lfp.get_samples()[c_start:c_end]
+            x_pos = [a + (j / convert) for j in range(len(lfp_sample))]
+            axes[i].plot(x_pos, lfp_sample, c="k")
+            axes[i].text(
+                0.01, 1, "Channel " + key,
+                transform=axes[i].transAxes, c="k")
+            axes[i].set_ylim(y_axis_min, y_axis_max)
+            axes[i].set_xlim(a, b)
+        print("Saving result to {}".format(out_name))
+        fig.savefig(out_name, dpi=dpi)
+        plt.close("all")
--- analysis/lfp_synchrony.py
+++ analysis/lfp_synchrony.py
@@ -1,10 +1,40 @@
 import os
+import json
 
-from api_utils import read_cfg, parse_args
+from api_utils import read_cfg, parse_args, setup_logging
+from general_lfp import get_all_lfp, plot_lfp
+
+from neurochat.nc_utils import get_all_files_in_dir, make_dir_if_not_exists
 
 
 def main(cfg, args, **kwargs):
-    pass
+    in_dir = cfg.get("Setup", "in_dir")
+    out_dir = cfg.get("Output", "out_dirname")
+    re_filter = cfg.get("Setup", "regex_filter")
+    re_filter = None if re_filter == "None" else re_filter
+    analysis_flags = json.loads(config.get("Setup", "analysis_flags"))
+
+    setup_logging(in_dir)
+    filenames = get_all_files_in_dir(
+        in_dir, ext=".set", recursive=True,
+        verbose=True, re_filter=re_filter, case_sensitive_ext=True)
+    filenames = [fname[:-4] for fname in filenames]
+    if len(filenames) == 0:
+        print("No set files found for analysis!")
+        exit(-1)
+    exit(-1)
+    for fname in filenames:
+        # TODO get the correct channels here based on the filename
+        channels = [1, 2, 16, 17, 31, 32]
+        lfp_odict = get_all_lfp(fname, channels=channels)
+        o_dir = os.path.join(in_dir, out_dir, os.path.basename(fname))
+
+        if analysis_flags[0]:
+            r = json.loads(config.get("LFP", "plot_time"))
+            seg_len = float(config.get("LFP", "plot_seg_length"))
+            make_dir_if_not_exists(os.path.join(o_dir, "dummy.txt"))
+            plot_lfp(
+                o_dir, lfp_odict, in_range=r, segment_length=seg_len)
 
 
 if __name__ == "__main__":
