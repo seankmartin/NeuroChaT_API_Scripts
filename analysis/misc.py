377fef4f0c1d55185994d2eba3a74d7270f869d3
Sean Martin <seankieran.m@hotmail.com>
2020-04-30 15:18:03 +0100

    Added function to get files from png name

--- analysis/misc.py
+++ analysis/misc.py
@@ -1,5 +1,9 @@
+import os
+
 import matplotlib.pyplot as plt
 import numpy as np
+
+from neurochat.nc_utils import get_all_files_in_dir
 
 
 def visualise_spacing(N=61, start=5, stop=10000):
@@ -11,3 +15,26 @@ def visualise_spacing(N=61, start=5, stop=10000):
     plt.ylim([-0.5, 1])
     print(x1)
     plt.show()
+
+
+def get_good_cells(directory):
+    with open("result.csv", "w") as out:
+        out.write("Location, Unit\n")
+        files = get_all_files_in_dir(directory, return_absolute=False)
+        good_cells = []
+        for f in files:
+            parts = os.path.splitext(f)[0].split("--")
+            last_bit = parts[-1]
+            last_split = last_bit.split("_")
+            name = "_".join(last_split[:-2])
+            name = name + "." + last_split[-2]
+            unit = int(last_split[-1])
+            path = os.path.join(*parts[:-1], name)
+            out.write("{}, {}\n".format(path, unit))
+            good_cells.append([path, unit])
+    return good_cells
+
+
+if __name__ == "__main__":
+    directory = r"D:\ATNx_CA1\final_plots\good"
+    get_good_cells(directory)
