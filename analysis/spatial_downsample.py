210791e5250d16819bbe9be8d02f5dfc3d326ad8
Sean Martin <seankieran.m@hotmail.com>
2020-05-01 15:14:24 +0100

    Downsampling works Need to test measures now

--- analysis/spatial_downsample.py
+++ analysis/spatial_downsample.py
@@ -13,7 +13,10 @@ from neurochat.nc_utils import histogram
 import neurochat.nc_plot as nc_plot
 
 
-def bin_downsample(self, ftimes, other_spatial, other_ftimes, final_bins):
+def bin_downsample(
+        self, ftimes, other_spatial, other_ftimes, final_bins,
+        initial_bin_size=30):
+    bin_size = initial_bin_size
     set_array = np.zeros(shape=(len(self._pos_x), 4))
     set_array[:, 0] = self._pos_x
     set_array[:, 1] = self._pos_y
@@ -22,39 +25,41 @@ def bin_downsample(self, ftimes, other_spatial, other_ftimes, final_bins):
     set_array[:, 3] = self._time
 
     pos_hist = np.histogram2d(
-        set_array[:, 0], set_array[:, 1], 10)
+        set_array[:, 0], set_array[:, 1], bin_size)
     pos_locs_x = np.searchsorted(
         pos_hist[1][1:], set_array[:, 0], side='left')
     pos_locs_y = np.searchsorted(
         pos_hist[2][1:], set_array[:, 1], side='left')
 
-    # set_array1 = np.zeros(shape=(len(other_spatial._pos_x), 4))
-    # set_array1[:, 0] = other_spatial._pos_x
-    # set_array1[:, 1] = other_spatial._pos_y
+    set_array1 = np.zeros(shape=(len(other_spatial._pos_x), 2))
+    set_array1[:, 0] = other_spatial._pos_x
+    set_array1[:, 1] = other_spatial._pos_y
     # spikes_in_bins = histogram(other_ftimes, bins=other_spatial.get_time())[0]
     # set_array1[:, 2] = spikes_in_bins
     # set_array1[:, 3] = other_spatial._time
-    # pos_hist1 = np.histogram2d(
-    #     set_array[:, 0], set_array[:, 1], 10)
+    pos_hist1 = np.histogram2d(
+        set_array1[:, 0], set_array1[:, 1], bin_size)
     # pos_locs_x1 = np.searchsorted(
-    #     pos_hist[1][1:], set_array[:, 0], side='left')
+    #     pos_hist1[1][1:], set_array1[:, 0], side='left')
     # pos_locs_y1 = np.searchsorted(
-    #     pos_hist[2][1:], set_array[:, 1], side='left')
+    #     pos_hist1[2][1:], set_array1[:, 1], side='left')
 
-    new_set = np.zeros(shape=(int(np.sum(pos_hist[0])), 4))
+    new_set = np.zeros(shape=(int(np.sum(pos_hist1[0])), 4))
     count = 0
 
     for i in range(pos_hist[0].shape[0]):
         for j in range(pos_hist[0].shape[1]):
-            amount = int(pos_hist[0][i, j])
+            amount1 = int(pos_hist1[0][i, j])
+            amount2 = int(pos_hist[0][i, j])
+            amount = min(amount1, amount2)
             subset = np.nonzero(np.logical_and(
                 pos_locs_x == i, pos_locs_y == j))[0]
             new_sample_idxs = np.random.choice(subset, amount)
             new_samples = set_array[new_sample_idxs]
             new_set[count:count + amount] = new_samples
             count += amount
-    print(np.histogram2d(
-        new_set[:, 0], new_set[:, 1], [pos_hist[1], pos_hist[2]])[0])
+    # print(np.histogram2d(
+    #     new_set[:, 0], new_set[:, 1], [pos_hist[1], pos_hist[2]])[0])
     spike_count = np.histogram2d(
         new_set[:, 1], new_set[:, 0], [final_bins[0], final_bins[1]],
         weights=new_set[:, 2])[0]
@@ -127,7 +132,8 @@ def downsample_place(self, ftimes, other_spatial, other_ftimes, **kwargs):
         self.get_time() >= lim[0], self.get_time() <= lim[1])]
 
     new_set, spike_count = self.bin_downsample(
-        ftimes, final_bins=[
+        ftimes, other_spatial, other_ftimes,
+        final_bins=[
             np.append(yedges, yedges[-1] + np.mean(np.diff(yedges))),
             np.append(xedges, xedges[-1] + np.mean(np.diff(xedges)))]
     )
@@ -242,12 +248,12 @@ NSpatial.downsample_place = downsample_place
 
 if __name__ == "__main__":
     spatial = NSpatial()
-    fname = r"D:\SubRet_recordings_imaging\muscimol_data\CanCSR8_muscimol\05102018\s1_smallsq_before\05102018_CanCSR8_smallsq_10_1_3.txt"
+    fname = r"D:\SubRet_recordings_imaging\muscimol_data\CanCSR8_muscimol\05102018\s3_after_smallsq\05102018_CanCSR8_smallsq_10_3_3.txt"
     spatial.set_filename(fname)
     spatial.load()
 
     spike = NSpike()
-    fname = r"D:\SubRet_recordings_imaging\muscimol_data\CanCSR8_muscimol\05102018\s1_smallsq_before\05102018_CanCSR8_smallsq_10_1.3"
+    fname = r"D:\SubRet_recordings_imaging\muscimol_data\CanCSR8_muscimol\05102018\s3_after_smallsq\05102018_CanCSR8_smallsq_10_3.3"
     spike.set_filename(fname)
     spike.load()
     spike.set_unit_no(1)
@@ -255,6 +261,45 @@ if __name__ == "__main__":
     p_data = spatial.place(spike.get_unit_stamp())
     fig = nc_plot.loc_firing(p_data)
     fig.savefig("normal.png")
-    p_down_data = spatial.downsample_place(spike.get_unit_stamp())
+    # By bonnevie this is likely stable, what about stability?
+    print(spatial.get_results()['Spatial Coherence'])
+    spatial._results.clear()
+
+    p_down_data = spatial.downsample_place(
+        spike.get_unit_stamp(), spatial, spike.get_unit_stamp())
     fig = nc_plot.loc_firing(p_down_data)
-    fig.savefig("down.png")
+    fig.savefig("down_v_self.png")
+
+    skaggs = np.zeros(shape=(50))
+    for i in range(50):
+        p_down_data = spatial.downsample_place(
+            spike.get_unit_stamp(), spatial, spike.get_unit_stamp())
+        skaggs[i] = spatial.get_results()['Spatial Coherence']
+        spatial._results.clear()
+    print(np.mean(skaggs))
+
+    spatial2 = NSpatial()
+    fname = r"D:\SubRet_recordings_imaging\muscimol_data\CanCSR8_muscimol\05102018\s4_big sq\05102018_CanCSR8_bigsq_10_4_3.txt"
+    spatial2.set_filename(fname)
+    spatial2.load()
+
+    spike2 = NSpike()
+    fname = r"D:\SubRet_recordings_imaging\muscimol_data\CanCSR8_muscimol\05102018\s4_big sq\05102018_CanCSR8_bigsq_10_4.3"
+    spike2.set_filename(fname)
+    spike2.load()
+    spike2.set_unit_no(6)
+
+    p_data = spatial2.place(spike.get_unit_stamp())
+    print(spatial2.get_results()['Spatial Skaggs'])
+    fig = nc_plot.loc_firing(p_data)
+    fig.savefig("normal2.png")
+
+    skaggs = np.zeros(shape=(50))
+    for i in range(50):
+        p_down_data = spatial.downsample_place(
+            spike.get_unit_stamp(), spatial2, spike2.get_unit_stamp())
+        skaggs[i] = spatial.get_results()['Spatial Coherence']
+        spatial._results.clear()
+    print(np.mean(skaggs))
+    fig = nc_plot.loc_firing(p_down_data)
+    fig.savefig("down_v_2.png")
