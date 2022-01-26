7d44756715f3687730a8506793a89d1af41ebdee
Sean Martin <seankieran.m@hotmail.com>
2022-01-26 13:06:19 +0000

    Update plot control

--- analysis/lfp_assorted_control.py
+++ analysis/lfp_assorted_control.py
@@ -5,25 +5,19 @@ from lfp_odict import LfpODict
 from lfp_plot import plot_lfp, plot_lfp_sig
 
 
-def main():
+def main(path1, path2, r, seg_len, channel_to_plot):
     # Set up the paths
     output_dir = Path(__file__).resolve().parent / "results"
     output_dir.mkdir(exist_ok=True)
-    main_path = Path("D:\Emanuela Rizzello data")
-    lfp_l2_ctrl_path = main_path / "to 09-2019" / "09072019-bt" / "09072019-bt-L2"
-    lfp_l2_dser_path = main_path / "to 09-2019" / "10072019-bt" / "10072019-bt-L2"
 
-    r = [20, 21]  # 4 seconds long LFP
-    seg_len = 1  # 2 second long segemnts
     theta_band = [5, 11]
     plot_ext = "png"
     sr = 250  # 250 Hz sample rate
-    # channel_to_plot = None  # Use None to check the channels
-    channel_to_plot = 1  # Or select a number after checking
-    
-    lfps = [lfp_l2_ctrl_path, lfp_l2_dser_path]
+
+    lfps = [path1, path2]
     types_ = ["CTRL", "D-SER"]
     for lfp_path, type_ in zip(lfps, types_):
+        b_name = lfp_path.name
         lfp_odict_l2 = LfpODict(
             str(lfp_path),
             channels="all",
@@ -34,7 +28,7 @@ def main():
             lfp_odict_l2.get_filt_signal(key=channel_to_plot),
             lfp_odict_l2.get_signal(key=channel_to_plot),
         ]
-        names = [f"{type_}_L2_theta", f"{type_}_L2_unfiltered"]
+        names = [f"{b_name}_{type_}_L2_theta", f"{b_name}_{type_}_L2_unfiltered"]
 
         for sig, name in zip(sigs, names):
             if channel_to_plot is None:
@@ -53,4 +47,21 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main_path = Path("D:\Emanuela Rizzello data")
+    lfp_l2_ctrl_path = main_path / "to 09-2019" / "09072019-bt" / "09072019-bt-L2"
+    lfp_l2_dser_path = main_path / "to 09-2019" / "10072019-bt" / "10072019-bt-L2"
+    r = [20, 21]  # 1 seconds long LFP
+    seg_len = 1  # 2 second long segemnts
+    channel_to_plot = 1  # Or select a number after checking
+    main(lfp_l2_ctrl_path, lfp_l2_dser_path, r, seg_len, channel_to_plot)
+
+    lfp_l2_ctrl_path = (
+        main_path / "to 09-2019" / "29072019-bt" / "29072019-bt-last30min-LFP"
+    )
+    lfp_l2_dser_path = (
+        main_path / "to 09-2019" / "30072019-bt" / "30072019-bt-last30min-LFP-DSER"
+    )
+    r = [20, 40]  # 20 seconds long LFP
+    seg_len = 20  # 20 second long segemnts
+    channel_to_plot = None  # Use None to check the channels
+    main(lfp_l2_ctrl_path, lfp_l2_dser_path, r, seg_len, channel_to_plot)
